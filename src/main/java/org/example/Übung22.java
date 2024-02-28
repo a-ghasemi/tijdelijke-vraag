cae273c4bae562c6faa12dda4973c3345c4a8f09
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-02-28 15:12:34 +0100

    add free names and format output

--- src/main/java/org/example/Übung22.java
+++ src/main/java/org/example/Übung22.java
@@ -3,10 +3,11 @@ package org.example;
 import java.util.Scanner;
 
 public class Übung22 {
-
     final static String[][] stundenplan = new String[5][6];
+    final static String[] taggen = new String[]{"Mo", "Di", "Mi", "Do", "Fr"};
 
     public static void main(String[] args) {
+        freieZeile();
 
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
@@ -34,24 +35,51 @@ public class Übung22 {
                     System.out.println("Ungültige Eingabe");
             }
         }
+
+        scanner.close();
     }
 
     public static void eingaben() {
+        int i,j;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bitte geben Sie die Stundenplan ein: ");
+        do {
+            System.out.println("Welcher tag soll gebucht werden? (1~5) ");
+            i = scanner.nextInt();
+        }while(i < 1 || i > 5);
+
+        do {
+            System.out.println("Welcher block soll gebucht werden? (1~6) ");
+            j = scanner.nextInt();
+        }while(j < 1 || j > 6);
+
+        System.out.println("Geben Sie den Namen ein: ");
+        stundenplan[i-1][j-1] = scanner.next().toString();
+    }
+
+    public static void  freieZeile() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 6; j++) {
-                System.out.println("Stunde " + (j + 1) + " Tag " + (i + 1) + " Eingabe: ");
-                stundenplan[i][j] = scanner.nextLine();
+                stundenplan[i][j] = "frei";
             }
         }
     }
 
     public static void anzeigen() {
+        System.out.print("\t\t\t");
+
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.println("Stunde " + (j + 1) + " Tag " + (i + 1) + " Stunde: " + stundenplan[i][j]);
+            System.out.format("%-20s\t", taggen[i]);
+        }
+        System.out.println();
+
+        for (int j = 0; j < 6; j++) {
+            System.out.format("Block %d\t\t", j + 1);
+
+            for (int i = 0; i < 5; i++) {
+                System.out.format("%-20s\t", stundenplan[i][j]);
             }
+            System.out.println();
         }
     }
 }
